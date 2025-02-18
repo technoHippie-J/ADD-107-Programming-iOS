64a2e7eb75bf738494904858da68c6039494e728
Tony Janus <tjanus@students.mchenry.edu>
2025-02-18 14:01:26 -0600

    module 1 homework upload

---
+++ module_1/1 Playground Basics/PlaygroundBasics.playground/Pages/Calculations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+34 + 76 + 230
+
+1503 - 305
+
+//: For multiplication, use the asterisk `*` :
+3 * 140
+//: For division, use the slash `/` :
+25 / 8
+//: For grouping, use parenthesis `( )` :
+(303 + 550 + 404) / 3
+
+42 * 42 / 4
+
+100 * 34 + (54 * 2)
+//: - experiment:\
+//: Practice using the playground like a calculator.
+//: Edit some of the existing expressions and add a few of your own.
+//: Notice that every time you make a change the results are updated in the sidebar.
+//:
+//: Next, learn how programmers can leave notes in code.
+//:
+//:[Previous](@previous)  |  page 3 of 7  |  [Next: Comments](@next)
---
+++ module_1/HelloWorldTonyJ.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+print("Hello World")
+
+let name = "Jon"
+let language = "Swift"
+var message1 = " Welcome to the wonderful world of "
+var message2 = "\(name), Welcome to the wonderful world of \(language)!"
+print(message2)
+print(name, message1, language, "!")
+
+let name1 = "Jon"
+let name2 = "Kailey"
+let name3 = "Kara"
+print(name1, name2, name3, separator:", ", terminator:"")
+
+print("\n")
+
+var line = ""
+print(name1, name2, name3, separator:", ", terminator:"", to:&line)
+print(line)
+
